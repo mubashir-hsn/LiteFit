1c0660ca52e2ef53675139d4bc4264da0dcb0ce2
mubashir-hsn <you@example.com>
2025-04-30 22:55:25 +0500

    main

--- backend/src/middleware/emailConfig.js
+++ backend/src/middleware/emailConfig.js
@@ -1,12 +1,15 @@
 import nodemailer from 'nodemailer'
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 export const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false, // true for port 465, false for other ports
   auth: {
-    user: 'cmubashar369@gmail.com' ,
-    pass: 'ymro npcs vlmr kgdd' ,
+    user: process.env.EMAIL ,
+    pass: process.env.PASS ,
   }
 });
 
