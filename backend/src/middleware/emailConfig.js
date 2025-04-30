31fb0774c539edbde5d2a28f067fba807e154777
Mubashar Hassan <164917849+mubashir-hsn@users.noreply.github.com>
2025-04-30 22:58:47 +0500

    Update emailConfig.js

--- backend/src/middleware/emailConfig.js
+++ backend/src/middleware/emailConfig.js
@@ -1,14 +1,18 @@
 import nodemailer from 'nodemailer'
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 export const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false, // true for port 465, false for other ports
   auth: {
-    user: '' ,
-    pass: '' ,
+    user: process.env.EMAIL ,
+    pass: process.env.PASS ,
   }
 });
+
 
 
   
