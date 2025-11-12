e3586feb5fad5fd6307087be3666366ba1b00b18
Mubashar Hassan <164917849+mubashir-hsn@users.noreply.github.com>
2025-11-13 00:42:48 +0500

    Update CORS origin to use environment variable

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cors(
     {
-        origin: 'http://localhost:5173',
+        origin: process.env.CLIENT_URL,
         credentials: true
     }
 ))
