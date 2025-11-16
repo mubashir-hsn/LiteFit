42d69f25f70802ea8b5038174294a3f4245f2cf7
Mubashar Hassan <164917849+mubashir-hsn@users.noreply.github.com>
2025-11-16 22:21:58 +0500

    Update input field styling in PromoBanner

--- frontend/src/pages/Home/PromoBanner.jsx
+++ frontend/src/pages/Home/PromoBanner.jsx
@@ -28,7 +28,7 @@ const PromoBanner = () => {
                   <h1 className="text-xl md:text-2xl text-center font-medium py-5">Subscribed And Stay Updated On Our <br /> Latest Collections.</h1>
 
                   <div className="flex flex-col md:block gap-2 justify-center items-center md:join mt-5 outline-none">
-                     <input className=" w-auto md:w-[27rem] p-2 text-sm md:text-lg md:p-4 focus:outline-none focus:border-none outline-none rounded-md md:join-item" required placeholder="Enter your email" />
+                     <input className=" w-auto md:w-[27rem] p-2 text-sm md:text-lg md:p-4 focus:outline-none focus:border-none outline-none rounded-md md:rounded-none md:join-item" required placeholder="Enter your email" />
                      <button className="uppercase font-medium tracking-wider text-sm md:text-lg px-2 py-2 md:px-4 md:py-4 text-center rounded-md md:join-item md:rounded-r-full bg-red-500 border-none hover:bg-indigo-500 text-white">Subscribe</button>
                   </div>
                </div>
